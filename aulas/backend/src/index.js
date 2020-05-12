ea6ddd067d04aa4caa52da177ef9c716f399f376
Fabio Araujo <fabioaraujods@gmail.com>
2020-05-11 23:29:09 -0300

    incluída explicação de como configurar um origin no cors

--- aulas/backend/src/index.js
+++ aulas/backend/src/index.js
@@ -4,6 +4,12 @@ const routes = require('./routes');
 
 const app = express()
 
+/**
+ * configurado uso do cors, em produção podemos configurar a url origin como abaixo
+ * app.use(cors({
+ *  origin: 'https://meuendreco.com.br'
+ * }));
+ */
 app.use(cors());
 //para avisar o express que usaremos json, com isso podemos receber request bodies
 app.use(express.json())
