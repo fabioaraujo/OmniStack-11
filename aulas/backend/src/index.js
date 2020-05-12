e47992eb41b77da07aa4548dc0952b79bb2c7908
Fabio Araujo <fabioaraujods@gmail.com>
2020-05-11 22:46:26 -0300

    incluído cors

--- aulas/backend/src/index.js
+++ aulas/backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express()
 
+app.use(cors());
 //para avisar o express que usaremos json, com isso podemos receber request bodies
 app.use(express.json())
 app.use(routes);
