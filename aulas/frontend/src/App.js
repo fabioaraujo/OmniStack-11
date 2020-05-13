492c2138d675b30f223f8a89a94feaebc090520a
Fabio Araujo <fabioaraujods@gmail.com>
2020-05-12 21:48:13 -0300

    criado componente de cabeçado

--- aulas/frontend/src/App.js
+++ aulas/frontend/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import Header from './Header';
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <Header>
+      Semana OmniStack
+    </Header>
   );
 }
 
