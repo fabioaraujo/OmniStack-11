157c1cfabfd191908eacb5911b4e239f4cacd15e
Fabio Araujo <fabioaraujods@gmail.com>
2020-05-15 14:55:20 -0300

    criada função de logout

--- aulas/frontend/src/pages/Profile/index.js
+++ aulas/frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -12,6 +12,8 @@ export default function Profile(){
     const [incidents, setIncidents] = useState([]);
     const ongId = localStorage.getItem('ongId');
     const ongName = localStorage.getItem('ongName');
+
+    const history = useHistory();
 
     useEffect (() => {
         api.get('profile', {
@@ -35,6 +37,12 @@ export default function Profile(){
         }
     }
 
+    function handleLogout(){
+        localStorage.clear();
+
+        history.push("/");
+    }
+
     return (
         <div className="profile-container">
             <header>
@@ -43,7 +51,7 @@ export default function Profile(){
                 <span>Bem vinda, {ongName}</span>
 
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-                <button type="button">
+                <button onClick={handleLogout} type="button">
                     <FiPower size={18} color="#e02041" />
                 </button>
             </header>
